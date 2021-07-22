5eebe165694a5dbf1eee92b34fe497336ac8fc07
gabmor38 <Gabriela.morenor@gmail.co>
2021-07-21 23:01:06 -0400

    updated code

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅✅✅ Assertion Passed: 1 === 1");
-  } else console.log("🛑🛑🛑🛑 Assertion Failed: Lighthouse Labs != Bootcamp");
+    console.log(`✅✅✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else console.log(`🛑🛑🛑🛑 Assertion Failed: ${actual} != ${expected}`);
   
 };
 
