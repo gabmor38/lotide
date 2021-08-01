6fe2601fc306fc7bf8d87de5378fab17e81b8781
gabmor38 <Gabriela.morenor@gmail.co>
2021-07-31 21:47:24 -0400

    updated version

--- eqObjects.js
+++ eqObjects.js
@@ -2,7 +2,7 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else console.log(`🛑🛑🛑🛑 Assertion Failed: ${actual} != ${expected}`);
-  
+
 };
 const eqArrays = function(arr1, arr2) {
   let result = true;
@@ -11,25 +11,38 @@ const eqArrays = function(arr1, arr2) {
       result = false;
     }
   }
-    if(result === true) {
-      console.log(`✅✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-    } else console.log(`🛑🛑🛑🛑 Assertion Failed: ${arr1} != ${arr2}`);
+  if (result === true) {
+    console.log(`✅✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else console.log(`🛑🛑🛑🛑 Assertion Failed: ${arr1} != ${arr2}`);
 };
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
-
-  if(Object.keys(object1).length !== Object.keys(object2).length) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  //determine if the  length in both objects are the same
+  if (keys1.length !== keys2.length) {
     return false;
   }
 
-  for (const key in object1 ) {
-    if(Array.isArray (Object.keys(object1)) === Array.isArray(Object.keys(object2))) 
-    //console.log(Object.keys(object1));
-  
-    return true;
- }
+  for (const key of keys1) {
+    const item1 = object1[key];
+    const item2 = object2[key];
+
+    if (Array.isArray(item1)) {
+      if (!eqArrays(item1, item2)) {
+        return false;
+      }
+      continue;
+    }
+    
+    if (item1 !== item2)  {
+      return false;
+    }
+
+  }
+  return true;
 
 };
 
@@ -43,7 +56,7 @@ eqArrays(eqObjects(ab, abc), false); // => false
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-eqArrays(eqObjects(cd, dc),true); // => true
+eqArrays(eqObjects(cd, dc), true); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 eqArrays(eqObjects(cd, cd2), false); // => false
