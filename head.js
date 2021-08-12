76892f8586b79959d5993ecc76f33f3f2ba14c58
gabmor38 <Gabriela.morenor@gmail.co>
2021-08-12 16:24:22 -0400

    refactor of Head

--- head.js
+++ head.js
@@ -1,14 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else console.log(`🛑🛑🛑🛑 Assertion Failed: ${actual} != ${expected}`);
-};
+
 const head = function(array) { 
   let firstItem = array [0]
   return firstItem
 };
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = head;
+
