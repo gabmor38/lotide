57cbec154ebc3def216a36d450ff3acdd2962b51
gabmor38 <Gabriela.morenor@gmail.co>
2021-08-12 22:17:21 -0400

    refactor middle.js

---
+++ test/middleTest.js
@@ -0,0 +1,9 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+const middle = require('../middle'); //import middle from middle
+
+
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5]);
+
+// console.log(middle([1, 2, 3]));//=>[2]
+
