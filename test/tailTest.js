cf75358fa16faaaa9508b09ba51811eee02b1342
gabmor38 <Gabriela.morenor@gmail.co>
2021-08-12 22:52:52 -0400

    refactor for M&C

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,30 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
+describe("#tail", () => {
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    const inputArray = [1, 2, 3];
+    assert.deepEqual(tail(inputArray), [2, 3]);
+  });
+
+  it("returns [5, 6] for [4, 5, 6]", () => {
+    const inputArray = [4, 5, 6]
+    assert.deepEqual(tail(inputArray), [5, 6]);
+  });
 
 
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+  it('returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    const inputArray = ["Hello", "Lighthouse", "Labs"];
+    assert.deepEqual(tail(inputArray), ["Lighthouse", "Labs"]);
+
+  });
+
+});
+
+
+
+
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
