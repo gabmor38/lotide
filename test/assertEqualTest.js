cd78b045890c1bdb437a8c80c351b0592d45ee86
gabmor38 <Gabriela.morenor@gmail.co>
2021-08-18 22:48:56 -0400

    updated version

--- test/assertEqualTest.js
+++
@@ -1,7 +0,0 @@
-const assertEqual = require('../assertEqual');
-
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-module.exports = assertEqual;
--- test/eqArraysTest.js
+++
@@ -1,9 +0,0 @@
-const assertEqual = require('../assertEqual');
-
-const eqArrays = require('../eqArrays');
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true);
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false);
