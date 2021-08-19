f5a9a7a95d1eb4f78ad5a6aa18c5252392f8c08f
gabmor38 <Gabriela.morenor@gmail.co>
2021-08-18 23:31:52 -0400

    Refactored

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -35,9 +35,4 @@ const assertObjectsEqual = function(actual, expected) {
   }
 };
 
-// const object1 = { a: '1', b: 2 };
-// const object2 = { b: 2, a: '1' };
-
-// assertObjectsEqual(object1, object2);
-
 module.exports = assertObjectsEqual;
