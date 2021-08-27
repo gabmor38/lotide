df3bde1b7537f5d94097ffb986e503bd330cc225
gabmor38 <Gabriela.morenor@gmail.co>
2021-08-26 21:27:46 -0400

    removed console.log lines

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -4,12 +4,12 @@ const assertArraysEqual = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       result = false;
-    } else { result = true;
+    } else {
+      result = true;
     }
-  };
+  }
   return result;
 };
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]), true); // => should PASS
 
 module.exports = assertArraysEqual;
 
