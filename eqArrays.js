e6965135db3eff9ff6194e5c42861fea271f6385
gabmor38 <Gabriela.morenor@gmail.co>
2021-08-26 21:05:57 -0400

    refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,17 @@
 
 const eqArrays = function(arr1, arr2) {
-  let result = true;
+  let result = "";
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      result = false;
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        result = true;
+      } else {
+        result = false;
+      }
     }
-  }
-    if(result === true) {
-      console.log(`✅✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-    } else console.log(`🛑🛑🛑🛑 Assertion Failed: ${arr1} != ${arr2}`);
+  } return result;
 };
+
 
 module.exports = eqArrays;
 
