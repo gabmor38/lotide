269dee9d65e2ae1ed335ae6f0feda786d99113fb
gabmor38 <Gabriela.morenor@gmail.co>
2021-08-26 21:16:40 -0400

    rremoved extra empty result array

--- flatten.js
+++ flatten.js
@@ -1,5 +1,4 @@
   const flatten = function(arr1, result = []) {  // arr1 is the main array and then result = [] is the nested array
-    let result = []// create function that takes array with nested arrays
     for (let i = 0, length = arr1.length; i < length; i++) { // loop through it to get all the values 
       const newFlattenArray = arr1[i]; // new number array 
       if (Array.isArray(newFlattenArray)) { //check if there are arrays within the array
